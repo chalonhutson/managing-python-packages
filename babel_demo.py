ad3d4614a244abe1c38660a5c78d0b71f1ad80ea
chalonhutson <chalonhutson@gmail.com>
2021-09-07 13:11:52 -0600

    First.

---
+++ babel_demo.py
@@ -0,0 +1,8 @@
+from babel import format_n, formaformat_number
+
+number = 12345678
+
+print("In the Netherlands we write",
+    format_number(number, locale = "en_US"),
+    "as",
+    format_number(number, locale= "nl_NL"))
